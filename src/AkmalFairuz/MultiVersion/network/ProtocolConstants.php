2252da36724799c7d1d4547f9c676222d6688ce8
ClydeNoIQ <86055764+ClydeplayzYT590@users.noreply.github.com>
2023-09-24 18:05:17 +0800

    Update ProtocolConstants.php

--- src/AkmalFairuz/MultiVersion/network/ProtocolConstants.php
+++ src/AkmalFairuz/MultiVersion/network/ProtocolConstants.php
@@ -8,30 +8,21 @@ use pocketmine\network\mcpe\protocol\ProtocolInfo;
 
 class ProtocolConstants{
 
-    public const BEDROCK_1_16_220 = 431;
-    public const BEDROCK_1_17_0 = 440;
-    public const BEDROCK_1_17_10 = 448;
-    public const BEDROCK_1_17_30 = 465;
-    public const BEDROCK_1_17_40 = 471;
-    public const BEDROCK_1_18_0 = 475;
+    public const BEDROCK_1_20_0 = 589;
+    public const BEDROCK_1_20_10 = 594;
+    public const BEDROCK_1_20_30 = 618;
 
     public const MINECRAFT_VERSION = [
-        self::BEDROCK_1_16_220 => "1.16.220",
-        self::BEDROCK_1_17_0 => "1.17.0",
-        self::BEDROCK_1_17_10 => "1.17.10",
-        self::BEDROCK_1_17_30 => "1.17.30",
-        self::BEDROCK_1_17_40 => "1.17.40",
-        self::BEDROCK_1_18_0 => "1.18.0",
+        self::BEDROCK_1_20_0 => "1.20.0",
+        self::BEDROCK_1_20_10 => "1.20.10",
+        self::BEDROCK_1_20_30 => "1.20.30",
         ProtocolInfo::CURRENT_PROTOCOL => ProtocolInfo::MINECRAFT_VERSION_NETWORK
     ];
 
     public const SUPPORTED_PROTOCOLS = [
-        self::BEDROCK_1_16_220,
-        self::BEDROCK_1_17_0,
-        self::BEDROCK_1_17_10,
-        self::BEDROCK_1_17_30,
-        self::BEDROCK_1_17_40,
-        self::BEDROCK_1_18_0,
+        self::BEDROCK_1_20_0,
+        self::BEDROCK_1_20_10,
+        self::BEDROCK_1_20_30,
         ProtocolInfo::CURRENT_PROTOCOL
     ];
 }
